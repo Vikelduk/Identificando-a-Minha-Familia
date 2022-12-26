9571b40ed75367f2243395886a9727233eb73d17
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2022-12-26 18:35:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-console.log("Eu tentei modificar o pedido do projeto, mas não achei muito legal")
+camera = document.getElementById("camera");
+      
+Webcam.attach( camera );      
+Webcam.set(
+  {
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+function take_snapshot()
+{
+  Webcam.snap(function(data_uri) 
+  {
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+  });
+}
+
+console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Reyiafnn6/model.json',modelLoaded);
+
+function modelLoaded() 
+{
+  console.log('Model Loaded!');
+}
+      
+function check()
+{
+  img = document.getElementById("selfie_image");
+  classifier.classify(img, gotResult); 
+}
+
+function gotResult(error, results) 
+{
+  if (error) 
+  {
+    console.error(error);
+  }
+   else
+  {
+    console.log(results);
+    document.getElementById("resultObjectName").innerHTML = results[0].label;
+    document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
